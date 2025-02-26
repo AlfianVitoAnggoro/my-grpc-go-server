8af3bef4da5e1e158b42c395cdec6dbe82ae9afb
AlfianVitoAnggoro <alfianvitoanggoro@gmail.com>
2025-02-26 17:50:16 +0700

    add service hello for server grpc

---
+++ internal/adapter/grpc/server.go
@@ -0,0 +1,48 @@
+package grpc
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/AlfianVitoAnggoro/my-grpc-go-server/internal/port"
+	"github.com/AlfianVitoAnggoro/my-grpc-proto/protogen/go/hello"
+	"google.golang.org/grpc"
+)
+
+type GrpcAdapter struct {
+	helloService port.HelloServicePort
+	grpcPort     int
+	server       *grpc.Server
+	hello.HelloServiceServer
+}
+
+func NewGrpcAdapter(helloService port.HelloServicePort, grpcPort int) *GrpcAdapter {
+	return &GrpcAdapter{
+		helloService: helloService,
+		grpcPort:     grpcPort,
+	}
+}
+
+func (a *GrpcAdapter) Run() {
+	var err error
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", a.grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("Server listening on port %v", a.grpcPort)
+
+	grpcServer := grpc.NewServer()
+	a.server = grpcServer
+
+	hello.RegisterHelloServiceServer(grpcServer, a)
+
+	if err = grpcServer.Serve(listen); err != nil {
+		log.Fatalf("failed to serve gRPC on port %d : %v\n", a.grpcPort, err)
+	}
+}
+
+func (a *GrpcAdapter) Stop() {
+	a.server.Stop()
+}
