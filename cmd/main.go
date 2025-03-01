cae3996e54c32c46f3843ab6717ff3be001a72b9
AlfianVitoAnggoro <alfianvitoanggoro@gmail.com>
2025-03-01 18:03:30 +0700

    add kurs mata uang

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"math/rand"
+	"time"
 
+	dbmigration "github.com/AlfianVitoAnggoro/my-grpc-go-server/db"
+	"github.com/AlfianVitoAnggoro/my-grpc-go-server/internal/application/domain/bank"
+	_ "github.com/jackc/pgx/v4/stdlib"
+
+	mydb "github.com/AlfianVitoAnggoro/my-grpc-go-server/internal/adapter/database"
 	mygrpc "github.com/AlfianVitoAnggoro/my-grpc-go-server/internal/adapter/grpc"
 	app "github.com/AlfianVitoAnggoro/my-grpc-go-server/internal/application"
 )
@@ -11,9 +19,65 @@ func main() {
 	log.SetFlags(0)
 	log.SetOutput(logWriter{})
 
-	hs := &app.HelloService{}
+	sqlDB, err := sql.Open("pgx", "postgres://postgres:password@localhost:5432/grpc?sslmode=disable")
 
-	grpcAdapter := mygrpc.NewGrpcAdapter(hs, 9090)
+	if err != nil {
+		log.Fatalln("Can't connect database", err)
+	}
+
+	dbmigration.Migrate(sqlDB)
+
+	databaseAdapter, err := mydb.NewDatabaseAdapter(sqlDB)
+
+	// if err != nil {
+	// 	log.Fatalln("Can't connect database adapter", err)
+	// }
+
+	// runDummyOrm(databaseAdapter)
+
+	hs := &app.HelloService{}
+	bs := app.NewBankService(databaseAdapter)
+
+	go generateExchangeRates(bs, "USD", "IDR", 5*time.Second)
+
+	grpcAdapter := mygrpc.NewGrpcAdapter(hs, bs, 9090)
 
 	grpcAdapter.Run()
+}
+
+// func runDummyOrm(da *mydb.DatabaseAdapter) {
+// 	now := time.Now()
+
+// 	uuid, _ := da.Save(
+// 		&mydb.DummyOrm{
+// 			UserId:    uuid.New(),
+// 			UserName:  "AlfianVitoAnggoro" + time.Now().Format("15:04:05"),
+// 			CreatedAt: now,
+// 			UpdatedAt: now,
+// 		},
+// 	)
+
+// 	res, _ := da.GetByUuid(&uuid)
+
+// 	log.Println("res :", res)
+// }
+
+func generateExchangeRates(bs *app.BankService, fromCurrency, toCurrency string, duration time.Duration) {
+	ticker := time.NewTicker(duration)
+
+	for range ticker.C {
+		now := time.Now()
+		validFrom := now.Truncate(time.Second).Add(3 * time.Second)
+		validTo := validFrom.Add(duration).Add(-1 * time.Millisecond)
+
+		dummyRate := bank.ExchangeRate{
+			FromCurrency:       fromCurrency,
+			ToCurrency:         toCurrency,
+			ValidFromTimestamp: validFrom,
+			ValidToTimestamp:   validTo,
+			Rate:               2000 + float64(rand.Intn(300)),
+		}
+
+		bs.CreateExchangeRate(dummyRate)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,40 @@ module github.com/AlfianVitoAnggoro/my-grpc-go-server
 go 1.23.5
 
 require (
-	github.com/AlfianVitoAnggoro/my-grpc-proto v0.0.4
+	github.com/AlfianVitoAnggoro/my-grpc-proto v0.0.6
+	github.com/golang-migrate/migrate/v4 v4.18.2
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v4 v4.18.3
+	google.golang.org/genproto v0.0.0-20250227231956-55c901821b1e
 	google.golang.org/grpc v1.70.0
+	gorm.io/gorm v1.25.12
 )
 
 require (
+	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.14.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250224174004-546df14abb99 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250227231956-55c901821b1e // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
+	gorm.io/driver/postgres v1.5.11
 )
